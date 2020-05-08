3fb0ec48078caf7f11225aa1d26b61224f5dbf22
esierka <esierka@objectivity.co.uk>
2020-05-08 13:54:27 +0200

    Init project

---
+++ SolutionAddedToRepo/SolutionAddedToRepo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolutionAddedToRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
